351e2ca4a0690ffd83ef465cdc40523bcd1a3b04
Hiroshi Takey F. (hiro2233) <htakey@gmail.com>
2021-09-13 16:21:24 -0400

    python: added prime number sucre snippet.

---
+++ src/python/prime_number_sucre.py
@@ -0,0 +1,49 @@
+# This code was published originally from here:
+# https://drive.google.com/file/d/1p2nQXw7cAWlFlbc3VRl1N0aKxTDZrCju/view
+
+
+from math import ceil, floor, sqrt
+import time
+
+def Cantidad(x):
+    pi_ = ceil(0.5 * floor((2 * x + (-1) ** x - 6 * c_(x) + 5) / 3))
+    return int(pi_)
+
+def prime_(x):
+    if ((((x - 1) / 3 )% 2) == 0) and ((x - 1) / 3 ).is_integer():
+        z = ceil((sqrt(x)-1) / 3)
+        for i in range(1, z + 1):
+            r = (2*(x - 1) - 6*i + (-1) ** i-1) / (6 * i + 3 - (-1) ** i)
+            if r.is_integer():
+                return False
+        return True
+    elif (((x - 2) / 3) % 2) != 0 and ((x - 2) / 3).is_integer():
+        z = ceil((sqrt(x-1)-1) / 3)
+        for i in range(1, z):
+            r = (2*(x - 2) - 6*i + (-1) ** i+1) / (6 * i + 3 - (-1) ** i)
+            if r.is_integer():
+                return False
+        return True
+    else:
+        return False
+
+def formula_1(x):
+    if (x < 5):
+        pass
+    count = 2
+    for i in range(5, x):
+        if prime_(i):
+            count += 1
+    return count
+
+values = [10, 100, 1000, 10000, 1000000]
+time.sleep(1)
+
+print("MI METODO")
+
+start_time = time.time()
+
+for v in values:
+    print("LA CANTIDAD DE PRIMOS MENORES A ", {v}, " ES: ", {formula_1(v)})
+
+print("TARDA ", time.time() - start_time, " SEGUNDOS ")
